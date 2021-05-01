e6a059d148677c9fa016d624939f6ca87101fca2
samvy94 <83478320+samvy94@users.noreply.github.com>
2021-05-01 14:32:55 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,12 @@ module.exports = {
             {name: '-greet', value: 'Great'}, 
             {name: '-kick @user', value: 'Kick someone out'},
             {name: '-clear num between 1-100. Ex) -clear 10', value: 'Clear chat history up to 100 messages'},
-            {name: '-play url OR -play keywords', value: 'Play Music'}
+            {name: '-play url OR -play keywords', value: 'Play Music'},
+            {name: '-definition', value : 'Displays the definition of the word'},
+            {name: '-getroles', value : 'Adds roles to the user'},
+            {name: '-image', value : 'Displays the searched image'},
+            {name: '-leave', value : 'Leaves the voice channel'}
+
         );
         message.channel.send(newEmbed)
     }
