8bf232bf2de6913afd13d0724b557533461612fc
Satoshiest <63283052+Satoshiest@users.noreply.github.com>
2021-05-01 23:07:25 -0400

    Update definition.js

--- commands/definition.js
+++ commands/definition.js
@@ -2,11 +2,6 @@ module.exports = {
     name: 'definition',
     description: 'Returns the definition of word from wikipedia.',
     async execute(client, message, cmd, args, Discord){
-        const word = args.join(' ');
-        result = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`)
-        .then(result => result.text())
-        .then(data => console.log(data))
-        .catch(error => console.error(error));
-        message.channel.send(result.definition);
+        message.channel.send('Sorry this command is not implemented yet...');
     }
 }
